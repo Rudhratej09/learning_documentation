b830c5cba142d9d22070fb9fb07cd10e0ced333c
Rudhratej Singh Likhari <rudhratejsingh6@gmail.com>
2025-01-30 18:53:14 +0530

    Update mainexpensetracker.cpp
    added code which i previously coded on vs code for a sample run i will update this later to a user/menu driven app

--- projects/c++/ExpenseTracker/mainexpensetracker.cpp
+++ projects/c++/ExpenseTracker/mainexpensetracker.cpp
@@ -1,1 +1,15 @@
-
+// main expense tracker cpp 
+#include<iostream>
+#include<string>
+#include<vector>
+#include "expensetracker.hpp"
+int main(){
+    std::vector <Expense> expenses;
+    Expense exp;
+    
+    exp.makeExpense("30-01-2025","17:32","Food",35);
+    expenses.push_back(exp);
+    exp.showExpense(expenses);
+    exp.removeExpense(expenses,"30-01-2025","17:32","Food",35);
+    
+}
