2ed013d293f76e028e43b876f3361805076e8589
Rudhratej Singh Likhari <rudhratejsingh6@gmail.com>
2025-01-31 13:49:25 +0530

    Update expensetracker.hpp
    created the header file for the function/method declarations for the expense class

--- projects/c++/ExpenseTracker/expensetracker.hpp
+++ projects/c++/ExpenseTracker/expensetracker.hpp
@@ -1,1 +1,17 @@
-
+// expense tracker hpp
+#include<iostream>
+#include<string>
+#include<vector>
+class Expense{
+    private:
+    std::string date;
+    std::string time;
+    std::string categories;
+    int amount;
+    public:
+    Expense(){}
+    void makeExpense(std::string dt,std::string t,std::string cat,int amt);
+    void removeExpense(std::vector<Expense>&expenses,std::string dt,std::string t,std::string cat,int amt);
+    void showExpense(std::vector<Expense>&expenses);
+    
+};
