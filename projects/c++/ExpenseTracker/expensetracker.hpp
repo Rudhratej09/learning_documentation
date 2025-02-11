cca18f26cab85883e428457ad754d13a8fdf742b
Rudhratej Singh Likhari <rudhratejsingh6@gmail.com>
2025-02-12 00:55:18 +0530

    Update expensetracker.hpp
    syntax/naming errors

--- projects/c++/ExpenseTracker/expensetracker.hpp
+++ projects/c++/ExpenseTracker/expensetracker.hpp
@@ -6,10 +6,10 @@ class Expense{
     public:
     std::string date;
     std::string time;
-    std::string categor;
+    std::string category;
     int amount;
     Expense() : date(""), time(""), amount(0), category(""){}
-    Expense(std::string dt,std::string t,int amt,std::string cat)
+    Expense(std::string dt,std::string t,int amt,std::string cat);
     void makeExpense(std::vector <Expense> &expenses);
     void removeExpense(std::vector<Expense>&expenses,std::string category,std::string time,std::string date,int amount);
     static void showExpense(std::vector<Expense>&expenses);
